90e0fb5bc706ff14978e700c8e56ce0e20beb023
Casey Li <casey.li@gmail.com>
2013-07-09 17:15:35 -0400

    Added Generic Modal Dialog.

---
+++ app/assets/javascripts/modal_dialog.js
@@ -0,0 +1,33 @@
+$(function(){
+	$("#close-generic-modal-dialog-link").click(hideGenericModalDialog);
+});
+
+function showGenericModalDialog(ihtml) {
+	
+	positionGenericModalDialog();
+	
+	var bodyHeight = $(document).height();
+	$("#generic-modal-dialog-background").height(bodyHeight);
+	$("#generic-modal-dialog-background").fadeIn(300);
+	$("#generic-modal-dialog-content").html(ihtml);
+	return false;
+}
+
+function hideGenericModalDialog() {
+	$("#generic-modal-dialog-background").fadeOut(300);
+	return false;
+}
+
+function positionGenericModalDialog() {
+	var _top = $("body").scrollTop();
+
+	/* IE Fix */
+	if(_top == 0) {
+		_top = (document.documentElement && document.documentElement.scrollTop) || 
+		              document.body.scrollTop;
+	}
+
+	_top = _top + 100;
+	_top = _top.toString() + "px";
+	$('#generic-modal-dialog-body').css('margin-top', _top);
+}
