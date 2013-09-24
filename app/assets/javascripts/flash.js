7f57c98706a808e3d8114bc270c70e79e8d34085
Casey Li <casey.li@gmail.com>
2013-09-24 16:17:55 -0400

    Added autoslide up of flash message.

--- app/assets/javascripts/flash.js
+++ app/assets/javascripts/flash.js
@@ -3,4 +3,11 @@ $(function(){
 		$(".flash").slideUp(100);
 		return false;
 	});
+	
+	/* AUTO SLIDE UP OF FLASH MESSAGE */
+	if($(".flash").length > 0) {
+	  setTimeout(function(){
+	    $(".flash").slideUp(100);
+	  }, 5000);
+	}
 });
